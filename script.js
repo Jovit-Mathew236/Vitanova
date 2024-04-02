b0e355de843195385da000df4f8a05afb8f727cd
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-04-03 01:47:27 +0530

    Update countdown timer display in index.html

--- script.js
+++ script.js
@@ -15,13 +15,30 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML =
-    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  // Output the result in the countdown element
+  var countdownElement = document.getElementById("countdown");
+  countdownElement.innerHTML = `
+        <div>
+            <h1>${days}</h1>
+            <h1 class="text-xs md:text-sm">DAYS</h1>
+        </div>
+        <div>
+            <h1>${hours}</h1>
+            <h1 class="text-xs md:text-sm">HOURS</h1>
+        </div>
+        <div>
+            <h1>${minutes}</h1>
+            <h1 class="text-xs md:text-sm">MINS</h1>
+        </div>
+        <div>
+            <h1>${seconds}</h1>
+            <h1 class="text-xs md:text-sm">SECS</h1>
+        </div>
+    `;
 
   // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    countdownElement.innerHTML = "EXPIRED";
   }
 }, 1000);
