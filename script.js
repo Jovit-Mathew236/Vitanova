629b1ed0f00e3e9a2d1969ae71559782bb2878bb
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-04-07 10:04:38 +0530

    Refactor authentication logic in script.js

--- script.js
+++ script.js
@@ -141,9 +141,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Fetch data only if the user is logged in and on the admin page
   auth.onAuthStateChanged((user) => {
-    if (user && window.location.pathname === "/admin.html") {
+    if (
+      (user && window.location.pathname === "/admin.html") ||
+      window.location.pathname === "/admin"
+    ) {
       fetchData();
-    } else if (!user && window.location.pathname === "/admin.html") {
+    } else if (
+      (!user && window.location.pathname === "/admin.html") ||
+      window.location.pathname === "/admin"
+    ) {
       // Redirect to login page if user is not logged in
       window.location.href = "login.html";
     }
